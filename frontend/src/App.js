23ba628385216635cb7b265f96191c828aa2b499
Basudev Bharat Bhushan <bharatbhushan173@gmail.com>
2022-11-02 15:19:15 +0530

    Frontend Testing 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,31 +8,63 @@ import PluggNFT from "./artifacts/contracts/PluggNFT.sol/PluggNFT.json" //Import
 const PluggNFT_ContractAddress = "0xc44Bb7bBe6bb5dF98F20A9Fc66430FC3321a56bB"
 
 function App() {
-    const [Owner, setOwner] = useState("")
+    const [ContractOwner, SetContractOwner] = useState("")
+    const [NFTOwner, setNFTOwner] = useState("")
+    const [NFTNumber, SetNFTNumber] = useState()
 
     //Request access to the user's Meta Mask Account
     async function requestAccount() {
         await window.ethereum.request({ method: "eth_requestAccounts" })
     }
 
-    async function showContractOwner() {
+    const showContractOwner = async () => {
         if (typeof window.ethereum !== "undefined") {
-            const provider = new ethers.providers.Web3Provider(window.ethereum)
+            const provider = new ethers.providers.JsonRpcProvider(
+                "https://polygon-mumbai.g.alchemy.com/v2/vimWVy8DiR0p0UJRgsb-JZZuJFgSDqg3"
+            )
             const contract = new ethers.Contract(PluggNFT_ContractAddress, PluggNFT.abi, provider)
             try {
-                const data = await contract.getOwnerAddress()
+                const ContractOwner = await contract.contractOwner()
+                SetContractOwner(ContractOwner)
+            } catch (error) {
+                console.log("Error:", error)
+            }
+        }
+    }
+
+    const showNFTOwner = async (e) => {
+        e.preventDefault()
+        if (typeof window.ethereum !== "undefined") {
+            const provider = new ethers.providers.JsonRpcProvider(
+                "https://polygon-mumbai.g.alchemy.com/v2/vimWVy8DiR0p0UJRgsb-JZZuJFgSDqg3"
+            )
+            const contract = new ethers.Contract(PluggNFT_ContractAddress, PluggNFT.abi, provider)
+            try {
+                const data = await contract.getOwnerAddress(NFTNumber)
                 console.log("Owner Address", data)
-                setOwner(data)
-            } catch (error) {}
+                setNFTOwner(data)
+            } catch (error) {
+                console.log("Error:", error)
+            }
         }
     }
 
     return (
         <div className="App">
             <h1>Basic Frontend Testing for NFT Application</h1>
-            <div className="btn" onClick={showContractOwner}>
-                <h3>Address:{Owner}</h3>
-                <button>Show Contract Owner</button>
+            <form onSubmit={showNFTOwner}>
+                <input
+                    placeholder="Enter NFT No."
+                    onChange={(e) => SetNFTNumber(e.target.value)}
+                    value={NFTNumber}
+                />
+                <button type="submit">Get NFT Owner</button>
+                <h4>Address: {NFTOwner}</h4>
+            </form>
+
+            <div>
+                <button onClick={showContractOwner}>Show Contract Owner</button>
+                <h3>Contract Owner: {ContractOwner}</h3>
             </div>
         </div>
     )
